25d13ed27c7f39c9f4a0b2a70825a100990d548b
ZaraKinYu <zaraknyu@gmail.com>
2026-01-06 02:57:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Repo Init
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
-repo sync -c -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune
+/opt/crave/resync.sh
 
 # Removing used directories
 rm -rf device/xiaomi/warm
@@ -14,20 +14,15 @@ rm -rf hardware/xiaomi
 git clone https://github.com/sm4635-development/android_device_xiaomi_warm -b lineage-23.0 device/xiaomi/warm
 git clone https://github.com/sm4635-development/android_vendor_xiaomi_warm -b lineage-23.0 vendor/xiaomi/warm
 git clone https://github.com/sm4635-development/android_device_xiaomi_warm-kernel -b lineage-23.0 device/xiaomi/warm-kernel
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/sm4635-development/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
 
 # Building
 source build/envsetup.sh
 lunch lineage_warm-bp2a-userdebug
 mka bacon -j$(nproc --all)
 
-# Upload file to TMP GOFILE
-echo "THIS IS NOW UPLOADING TO GOFILE IF BALWINDAR IS SLEEPING AF LMAOOOOOOOOOOOOOOOOOO"
-echo "STILL IN PROGESS"
-
-
+# Upload to GAYFILE lol
 curl -F "file=@out/target/product/warm/lineage-23.0*UNOFFICIAL*.zip" https://store2.gofile.io/uploadFile
+echo "DONE UPLOADING TO GOFILE"
 
-
-echo "DONE UPLOADING TO GOFILE, BALWINDAR,thoda kashth krke link bhej dijiye, lazy boi"
