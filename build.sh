9c62a6ae31a114f9d836fe0e835de7a6a544ef0e
ZaraKinYu <zaraknyu@gmail.com>
2026-01-09 22:23:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ rm -rf vendor/xiaomi/warm
 rm -rf hardware/qcom-caf/common
 rm -rf hardware/xiaomi
 
-git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b lineage-23.0 device/xiaomi/warm
-git clone https://github.com/zaraknyu/android_vendor_xiaomi_warm -b lineage-23.0 vendor/xiaomi/warm
-git clone https://github.com/zaraknyu/android_device_xiaomi_warm-kernel -b lineage-23.0 device/xiaomi/warm-kernel
-git clone https://github.com/zaraknyu/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b lineage-23.1 device/xiaomi/warm
+git clone https://github.com/zaraknyu/android_vendor_xiaomi_warm -b lineage-23.1 vendor/xiaomi/warm
+git clone https://github.com/zaraknyu/android_device_xiaomi_warm-kernel -b lineage-23.1 device/xiaomi/warm-kernel
+git clone https://github.com/zaraknyu/android_hardware_qcom-caf_common -b lineage-23.1 hardware/qcom-caf/common
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi
 
 # Building
 source build/envsetup.sh
